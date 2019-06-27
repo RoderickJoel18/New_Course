b74c0ffa2ff9c3d100eb75e4a9923a5380f9938c
roderick <roderickjaffeth@gmail.com>
2019-06-27 01:58:09 -0500

    Aprediendo la clase Math

---
+++ 2. Clase Math/src/ClaseMath.java
@@ -0,0 +1,21 @@
+public class ClaseMath {
+    public static void main(String[] args) {
+
+        //Metodo sqrt para raiz cuadrada
+        double raiz = Math.sqrt(9);
+        System.out.println(raiz);
+
+        //Metodo round para redondeo
+        double num = 5.85;
+        int resultado = (int) Math.round(num);
+        System.out.println(resultado);
+
+        //Metodo pow para exponentes
+        double base = 5;
+        double exponente = 3;
+
+        int respuesta = (int) Math.pow(base, exponente);
+        System.out.println(respuesta);
+
+    }
+}
